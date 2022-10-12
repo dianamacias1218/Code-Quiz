b0c8859e84d7b0e5fc89b16e9cb95a9ddc78b706
dianamacias1218 <95941568+dianamacias1218@users.noreply.github.com>
2022-10-11 22:20:19 -0500

    updated file

---
+++ script.js
@@ -0,0 +1,12 @@
+//variables
+var questions
+var highscores
+const startButton = document.getElementById ('button')
+
+
+/*questions
+create functions*/
+
+//initials and submission 
+
+//highscores
